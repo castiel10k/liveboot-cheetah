40322babf3040f078577dfa209aa50640a1dd2b0
castiel10k <71992808+castiel10k@users.noreply.github.com>
2024-01-05 15:51:56 -0500

    test

--- liveBootAni2/src/main/java/eu/chainfire/liveboot/MainActivity.java
+++ liveBootAni2/src/main/java/eu/chainfire/liveboot/MainActivity.java
@@ -16,16 +16,32 @@
 
 package eu.chainfire.liveboot;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Surface;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends Activity implements InAppPurchases.OnPurchaseListener {
     private Handler handler = new Handler();
@@ -47,13 +63,15 @@ public class MainActivity extends Activity implements InAppPurchases.OnPurchaseL
         } else { 
           return Configuration.ORIENTATION_PORTRAIT;
         }
-    }    
-    
+    }
+    private static final int REQUEST_WRITE_EXTERNAL_STORAGE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setRequestedOrientation(getDeviceDefaultOrientation());
         setContentView(R.layout.activity_main);
+
     }
 
     private String getScreenSize() {
@@ -62,7 +80,7 @@ public class MainActivity extends Activity implements InAppPurchases.OnPurchaseL
         int height = displayMetrics.heightPixels;
         int width = displayMetrics.widthPixels;
         height = height + getNavigationBarHeight();
-        return height +" "+ width;
+        return height +";"+ width;
     }
 
     private int getNavigationBarHeight() {
@@ -94,14 +112,126 @@ public class MainActivity extends Activity implements InAppPurchases.OnPurchaseL
             }, 60000);
         }
     }
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, proceed with writing the file
+                writeToFileInDocuments();
+            } else {
+                // Permission denied, handle it accordingly (e.g., show a message to the user)
+            }
+        }
+    }
     @Override
     protected void onStart() {
         super.onStart();
+
         autoExit = true;
         autoExitCounter++;
         String test = getScreenSize();
         Log.d("TEST", test);
+        
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it from the user
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_EXTERNAL_STORAGE
+            );
+        } else {
+            // Permission is already granted, proceed with writing the file
+            writeToFileInDocuments();
+        }
+        /** String jsonString;
+        try {
+            jsonString = getJsonFromString(test);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        assert jsonString != null;
+        Log.d("TEST2", jsonString);
+        saveJsonToFile(jsonString, "data.txt"); **/
+
+
+
+
+    }
+
+    private void writeToFileInDocuments() {
+        String fileName = "example.txt";
+        String content = "Hello, this is the content of the file.";
+
+        // Get the Documents directory on external storage
+        File documentsDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+
+        // Create a File object for the specified file in the Documents directory
+        File file = new File(documentsDirectory, fileName);
+
+        try {
+            // Create a FileOutputStream for the file
+            FileOutputStream fos = new FileOutputStream(file);
+
+            // Write the content to the file
+            fos.write(content.getBytes());
+
+            // Close the FileOutputStream
+            fos.close();
+
+            // Log success
+            Log.d("File Write", "File written to Documents directory: " + file.getAbsolutePath());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle IOException if there is an issue writing the file
+        }
+    }
+
+    private void saveJsonToFile(String jsonString, String fileName) {
+
+        try {
+            // Accessing the application's internal storage
+            FileOutputStream fos = openFileOutput(fileName, Context.MODE_PRIVATE);
+
+            // Writing the JSON string to the file
+            fos.write(jsonString.getBytes());
+
+            // Closing the file output stream
+            fos.close();
+
+            // Optional: Notify the user that the operation was successful
+
+            Toast.makeText(this, "JSON data saved to " + Context.MODE_PRIVATE +" "+ fileName, Toast.LENGTH_SHORT).show();
+            Log.d("TEST3",Context.MODE_PRIVATE +""+ fileName );
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle any exceptions that may occur during file writing
+        }
+    }
+
+    private String getJsonFromString(String test) throws JSONException {
+        String jsonString;
+        try {
+            // Creating a JSON object
+            JSONObject jsonObject = new JSONObject();
+            String[] values = test.split(";");
+            // Adding key-value pairs to the JSON object
+            jsonObject.put("height", values[0]);
+            jsonObject.put("width", values[1]);
+
+            // Converting the JSON object to a string
+            jsonString = jsonObject.toString();
+
+            // Printing the JSON string
+            Log.d("JSON Example", jsonString);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return jsonString;
     }
 
     public void setAutoExit(boolean autoExit) {
